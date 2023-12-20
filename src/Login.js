ea9a12ef9bc2f8ace057f11f18d4d1f0cf8c539f
Bhatia <anubhatia@deloitte.com>
2023-12-20 14:32:56 +0530

    Added to Github

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='Login'>
+        <p>Sign in with Google To Continue</p>
+        <button className='login-with-google-btn'>Sign in With Google</button>
+      
+    </div>
+  )
+}
